cfef1889d979ca50251bf1d6c8d19a799193fda8
mariana.solano <mariana.solano@datacivica.org>
2020-07-20 11:01:16 -0500

    cambios finales

--- descrip/src/grafs-edades.R
+++ descrip/src/grafs-edades.R
@@ -10,7 +10,7 @@
 
 rm(list=ls())
 
-pacman::p_load(tidyverse, ggrepel, sf, lwgeom, yaml, here, ggmosaic, treemapify)
+pacman::p_load(tidyverse, ggrepel, sf, lwgeom, yaml, here, ggmosaic, treemapify, svglite)
 
 
 tema <- theme_minimal() +
@@ -31,10 +31,8 @@ fill_muchos <-  c("#9e0142", "#d53e4f", "#f46d43", "#fdae61", "#fee08b", "#ffffb
 
 
 files <- list(datos = here("/clean-data/output/cenapi-rnpdno-edades.rds"),
-              g_e1 = here("/descrip/output/g_e1.png"),
-              g_e2 = here("/descrip/output/g_e2.png"),
-              g_e3 = here("/descrip/output/g_e3.png"),
-              g_e4 = here("/descrip/output/g_e4.png"))
+              g_e1 = here("/descrip/output/g_e1.svg"),
+              g_e3 = here("/descrip/output/g_e3.svg"))
 
 datos <- readRDS(files$datos)
 
@@ -42,7 +40,8 @@ datos <- readRDS(files$datos)
 datos$gpo_edad <- ifelse(is.na(datos$gpo_edad), "Sin especificar", datos$gpo_edad)
 
 datos <- datos %>% 
-         mutate(gpo_edad = factor(gpo_edad, levels = c("De 0 a 4 años", "De 5 a 9 años", 
+         mutate(gpo_edad = factor(gpo_edad, levels = c("Sin especificar",
+                                                       "De 0 a 4 años", "De 5 a 9 años", 
                                                        "De 10 a 14 años", "De 15 a 19 años", 
                                                        "De 20 a 24 años", "De 25 a 29 años", 
                                                        "De 30 a 34 años","De 35 a 39 años",
@@ -50,7 +49,7 @@ datos <- datos %>%
                                                        "De 50 a 54 años", "De 55 a 59 años", 
                                                        "De 60 a 64 años", "De 65 a 69 años", 
                                                        "De 70 a 74 años","De 75 a 79 años",
-                                                       "De 80 años en adelante", "Sin especificar")))
+                                                       "De 80 años en adelante")))
 
 ##================== Gráficas ==================##
 tempo <- datos %>% 
@@ -67,7 +66,7 @@ ggplot(tempo,aes(x = gpo_edad, y = porcent, fill = status))+
        geom_label(aes(label = paste0(porcent, "%")), position = position_stack(vjust = 0.5),
                   family = "Barlow Condensed", color = "black", angle = 0, size = 3) +
        labs(title = "Estatus de las personas desaparecidas, no localizadas y localizadas en México", 
-            subtitle = "por edad y fuente de registro", 
+            subtitle = "Por grupo de edad y fuente de registro", 
             caption = "Fuente: Elaboración propia con datos del RNPDNO y CENAPI", 
             fill = "", x = "", y = "") +
        tema 
@@ -80,17 +79,6 @@ tempo <- datos %>%
          mutate(denomin = sum(total), 
                 porcent = round(total / denomin * 100, 1)) 
 
-ggplot(tempo, aes(area = total, fill = gpo_edad, label = paste0(porcent,"%\n", total))) +
-       geom_treemap() + 
-       facet_wrap(fuente~sexo) +
-       geom_treemap_text(place = "centre", grow = F, color ="black") +
-       labs(title = "Porcentajes por edad de las personas desaparecidas, no localizadas y localizadas en México", 
-            subtitle = "por fuente de registro", 
-            caption = "Fuente: Elaboración propia con datos del RNPDNO y CENAPI", fill = "") +
-       tema + 
-       coord_fixed()
-ggsave(files$g_e2, width = 18, height = 15)
-
 ggplot(tempo,aes(x = gpo_edad, y = porcent, fill = sexo))+
        geom_bar(position = "stack", stat = "identity") + 
        facet_wrap(~fuente) +
@@ -99,7 +87,7 @@ ggplot(tempo,aes(x = gpo_edad, y = porcent, fill = sexo))+
        geom_text(aes(label = paste0(porcent, "%")), position = position_stack(vjust = 0.5),
                   family = "Barlow Condensed", color = "black", angle = 0, size = 3) +
        labs(title = "Porcentaje de las personas desaparecidas, no localizadas y localizadas en México", 
-            subtitle = "por grupo de edad y fuente de registro", 
+            subtitle = "Por grupo de edad y fuente de registro", 
             caption = "Fuente: Elaboración propia con datos del RNPDNO y CENAPI", 
             fill = "", x = "", y = "") +
   tema 
